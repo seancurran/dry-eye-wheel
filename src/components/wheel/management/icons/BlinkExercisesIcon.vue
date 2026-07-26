2ba46c288cd228e66656a1beba44257683b50ad1
Sean Curran <sean@studiostray.com>
2026-07-27 11:19:15 +1200

    fix: nudge Ukrainian Blink Exercises further right

--- src/components/wheel/management/icons/BlinkExercisesIcon.vue
+++ src/components/wheel/management/icons/BlinkExercisesIcon.vue
@@ -7,7 +7,7 @@ const controlsStore = useControlsStore();
     <g
         :class="{ 'svg-active': controlsStore.selectedItem === 'blink-exercises' }"
         class="svg-icon svg-fill-path svg-fill-circle"
-        :style="{ transform: $selectedLanguage.value === 'uk' ? 'translate(8px, 0px)' : 'translate(0px, 0px)' }">
+        :style="{ transform: $selectedLanguage.value === 'uk' ? 'translate(16px, 0px)' : 'translate(0px, 0px)' }">
         <!-- Icon -->
         <g>
             <g>
