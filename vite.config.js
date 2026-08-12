aba5acba351f70ecf003bdc8995d0a54f483bc07
Sean Curran <sean@studiostray.com>
2026-08-12 18:07:15 +1200

    ran build

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+    base: '/dry-eye-wheel/',
     plugins: [
         vue(),
         svgLoader({
